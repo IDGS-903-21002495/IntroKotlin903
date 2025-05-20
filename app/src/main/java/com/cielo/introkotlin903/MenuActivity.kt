b058677c29069d9efe79ef86ff6bd0031ebcb38f
IDGS-803-21002495 <21002495@alumnos.utleon.edu.mx>
2025-05-19 19:46:12 -0600

    OperasBasicas

---
+++ app/src/main/java/com/cielo/introkotlin903/MenuActivity.kt
@@ -0,0 +1,46 @@
+package com.cielo.introkotlin903
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.cielo.introkotlin903.practica1.SaludoActivity
+import com.cielo.introkotlin903.practica2.OperasActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu2)
+        val btnSaludo = findViewById<Button>(R.id.btn1)
+        val btnCalcular = findViewById<Button>(R.id.btn2)
+
+
+        btnSaludo.setOnClickListener{
+            navegateToSaludo()
+        }
+
+        btnCalcular.setOnClickListener {
+            navegateToCalculo()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun navegateToCalculo() {
+        val intent = Intent(this,OperasActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navegateToSaludo() {
+        val intent = Intent(this,SaludoActivity::class.java)
+        startActivity(intent)
+    }
+}
